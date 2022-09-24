95756f0e83c3fdd4f05a0b764dfed8f989c9394f
MaksymMakovskyi <5402029@stud.nau.edu.ua>
2022-09-25 01:31:17 +0300

    First Commit
    First Commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LabWork2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть кiлькiсть студентiв: ");
+            int amountOfStudents = Convert.ToInt32(Console.ReadLine());
+            string f;
+            int v;
+            int o;
+            int vSum = 0;
+            int oSum = 0;
+            int p1Sum = 0;
+            //Створення масиву об'єктів
+            SecondName[] ArrayOfObjects = new SecondName[amountOfStudents];
+            for (int i = 0; i < amountOfStudents; i++)
+            {
+                //Одержання інформації про студентів з консолі
+                Console.Write($"Введiть прiзвище {i+1}-го студента: ");
+                f = Console.ReadLine();
+                Console.Write($"Введiть загальну кiлькiсть пропущених {i + 1}-им студентом годин: ");
+                v = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Введiть кiлькiсть пропущених {i + 1}-им студентом годин (виправдано): ");
+                o = Convert.ToInt32(Console.ReadLine());
+                if (o>v)
+                {
+                    Console.WriteLine("Кiлькiсть прогуляних годин не може бути вiд'ємною");
+                    break;
+                }
+                ArrayOfObjects[i] = new SecondName(f, v, o);
+            }
+            for (int i = 0; i < amountOfStudents; i++)
+            {
+                //Обчислення загальної кількості пропусків всіх студентів
+                vSum += ArrayOfObjects[i].V;
+                oSum += ArrayOfObjects[i].O;
+                p1Sum += ArrayOfObjects[i].P1;
+            }
+            //Вивід заголовка таблиці в консоль
+            Console.WriteLine();
+            Console.WriteLine(" №\t    Прiзвище\t\tПропущено, год.\t\t\tПропуск\t\t");
+            Console.WriteLine(" з/п\t\t\t\tусього\tвиправдано\tу годинах\tу %\t");
+            Console.WriteLine();
+            //Вивід інформації в консоль
+            for (int i = 0; i < amountOfStudents; i++)
+            {
+                Console.WriteLine($" {i+1,4} {ArrayOfObjects[i].F,18} {ArrayOfObjects[i].V,13} " +
+                    $"{ArrayOfObjects[i].O,11} {ArrayOfObjects[i].P1,14} {ArrayOfObjects[i].P2,9}");
+            }
+            //Вивід суми годин
+            Console.WriteLine($"\t    Разом:{vSum,20} {oSum,11} {vSum,14}");
+        }
+    }
+}
---
+++ SecondName.cs
@@ -0,0 +1,45 @@
+﻿namespace LabWork2._1
+{
+    internal class SecondName
+    {
+        string f;
+        int v;
+        int o;
+        int p1;
+        int p2;
+        //Конструктор
+        public SecondName(string f, int v, int o)
+        {
+            this.f = f;
+            this.v = v;
+            this.o = o;
+            p1 = v - o;
+            p2 = p1 * 100 / v;
+        }
+        public string F
+        {
+            get { return f; }
+            set { f = value; }
+        }
+        public int V
+        {
+            get { return v; }
+            set { v = value; }
+        }
+        public int O
+        {
+            get { return o; }
+            set { o = value; }
+        }
+        public int P1
+        {
+            get { return p1; }
+            set { p1 = value; }
+        }
+        public int P2
+        {
+            get { return p2; }
+            set { p2 = value; }
+        }
+    }
+}
